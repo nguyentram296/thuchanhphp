1cf0a2af4f78c4353b2064f28478e416a436e5bf
Nguyen Tram <nguyentram10b1@gmail.com>
2020-06-17 01:41:55 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="TangXuanDuc" />
+  <link href="/Lab5/site.css" rel="stylesheet"/>
+  <link href="/Lab5/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <title>Project Training- Website Bán Hàng</title>
+</head>
+<body>
+<div id="wrapper">
+  <h2>Project Training- Xây dựng Website Bán Hàng</h2>
+  <div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="/Lab5/list_product.php"  class="navbar-brand"><button type="button" class="btn btn-danger  ">Danh sách sản phẩm </button></a>
+        <a href="/Lab5/add_product.php" class="navbar-brand"><button type="button" class="btn btn-danger">Thêm sản phẩm </button></a>
+      </div>
+    </div>
+  </div>
